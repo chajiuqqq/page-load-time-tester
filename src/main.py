3b4b3435fbd08ce262f31730912e714fef6bcaec
chajiuqqq <chajiuqqq@gmail.com>
2023-12-30 12:04:37 +0800

    feat: add allow insecure option & test multi urls

--- src/main.py
+++ src/main.py
@@ -9,8 +9,8 @@ logging.basicConfig(level=logging.INFO)
 def main():
 	parser = argparse.ArgumentParser(description='Page loading performance test')
 	parser.add_argument('chromedriver', metavar='DRIVER', type=str, help='Relative path to chromedriver')
-	parser.add_argument('url', metavar='URL', type=str, help='URL of page to do performance test')
 	parser.add_argument('log', metavar="LOG", type=str, help='Name of output json log file')
+	parser.add_argument('--urls',nargs='+', metavar='URLS', help='URLS of page to do performance test')
 	parser.add_argument('--loop', default=1, type=int, help='Number of times to reload page')
 
 	args = parser.parse_args()
@@ -19,7 +19,7 @@ def main():
 		raise FileNotFoundError('Failed to access browser driver at designated path {}'.format(args.chromedriver))
 
 	performance_test = performanceTest(driver_path=args.chromedriver,
-									   url=args.url, 
+									   urls=args.urls, 
 									   log_file=args.log,
 									   loop=args.loop)
 	performance_test.run()
--- src/perf_test.py
+++ src/perf_test.py
@@ -12,17 +12,15 @@ logging.basicConfig(level=logging.INFO)
 
 class performanceTest:
 
-	def __init__(self, url, log_file, driver_path, loop):
-		self.url = url
+	def __init__(self, urls, log_file, driver_path, loop):
+		self.urls = urls
 		self.log_file = log_file
 		self.driver_path = driver_path
 		self.loop = loop
 
 		self.log_dir = './result'
 		self.__options = ''
-		self.output_json = {"url": self.url,
-						    "averageLoadingTime": 0,
-						    "resultSet": []}
+		self.output_json_list = []
 
 		self.load_driver_options()
 
@@ -33,33 +31,41 @@ class performanceTest:
 		driver = webdriver.Chrome(desired_capabilities=caps,
 								  executable_path=self.driver_path,
 								  chrome_options=self.__options)
+		for url in self.urls:
+			print(f'plt testing {url}...')
+			attemp_number = 1
+			total_loading_time = 0
+			output_json_item = {"url": "",
+						    "averageLoadingTime": 0,
+						    "resultSet": []}
+			output_json_item['url']=url
+			
+			while attemp_number <= self.loop:
+				# Calculate loading time
+				time_start = time.time()
+				driver.get(url)
+				performance_data = driver.execute_script("return window.performance.getEntries();")
+				print(f'performance_data for {url}\n')
+				print(performance_data)
+				time_end = time.time()
 
-		attemp_number = 1
-		total_loading_time = 0
+				reduced_performance_data = self.reduce_data_set(performance_data, attemp_number)
+				reduced_performance_data['loadingTime'] = (time_end - time_start) * 1000
 
-		while attemp_number <= self.loop:
-			# Calculate loading time
-			time_start = time.time()
-			driver.get(self.url)
-			performance_data = driver.execute_script("return window.performance.getEntries();")
-			time_end = time.time()
+				# Append current attempt at loading to output data
+				total_loading_time += reduced_performance_data['loadingTime']
+				output_json_item['resultSet'].append(reduced_performance_data)
 
-			reduced_performance_data = self.reduce_data_set(performance_data, attemp_number)
-			reduced_performance_data['loadingTime'] = (time_end - time_start) * 1000
+				attemp_number += 1	
 
-			# Append current attempt at loading to output data
-			total_loading_time += reduced_performance_data['loadingTime']
-			self.output_json['resultSet'].append(reduced_performance_data)
-
-			attemp_number += 1	
-
-		self.output_json['averageLoadingTime'] = total_loading_time / self.loop
+			output_json_item['averageLoadingTime'] = total_loading_time / self.loop
+			self.output_json_list.append(output_json_item)
 
 		if not os.path.exists(self.log_dir):
 			os.makedirs(self.log_dir)
 
 		with open(os.path.join(self.log_dir, self.log_file), 'w') as f:
-		    json.dump(self.output_json, f, indent=4)
+		    json.dump(self.output_json_list, f, indent=4)
 
 		driver.close()
 
@@ -100,4 +106,6 @@ class performanceTest:
 		self.__options.add_argument('--headless')
 		self.__options.add_argument('--disable-gpu')
 		self.__options.add_argument('--disable-cache')
+		self.__options.add_argument('--ignore-certificate-errors')
+		
 		self.__options.binary_location = "/usr/bin/google-chrome"
